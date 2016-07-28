a6b059fecd8143017bc8b034ddcaed2ef2df86c1
kinghost <kingraser@gmail.com>
2016-07-28 14:43:19 +0800

    refactor authority

--- src/main/java/com/sogou/pay/remit/api/CronController.java
+++ src/main/java/com/sogou/pay/remit/api/CronController.java
@@ -5,6 +5,12 @@
  */
 package com.sogou.pay.remit.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.hibernate.validator.constraints.NotBlank;
 import org.jsondoc.core.annotation.Api;
 import org.jsondoc.core.annotation.ApiMethod;
@@ -12,16 +18,22 @@ import org.quartz.CronExpression;
 import org.quartz.TriggerKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sogou.pay.remit.api.JobController.JobName;
+import com.sogou.pay.remit.entity.User.Role;
 import com.sogou.pay.remit.model.ApiResult;
+
+import commons.utils.Tuple2;
 
 //--------------------- Change Logs----------------------
 //@author wangwenlong Initial Created at 2016年7月22日;
@@ -29,9 +41,11 @@ import com.sogou.pay.remit.model.ApiResult;
 @Api(name = "cron API", description = "Read/Write/Update/ the cron")
 @RestController
 @RequestMapping("/api")
-public class CronController {
+public class CronController implements InitializingBean {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CronController.class);
+
+  private static Map<JobName, Tuple2<CronTriggerFactoryBean, TriggerKey>> JOB_MAP = new HashMap<>();
 
   @Autowired
   @Qualifier("payTrigger")
@@ -44,20 +58,14 @@ public class CronController {
   @Autowired
   private SchedulerFactoryBean factory;
 
-  private static final TriggerKey PAY_TRIGGER_KEY = new TriggerKey("payTrigger");
-
-  private static final TriggerKey QUERY_TRIGGER_KEY = new TriggerKey("queryTrigger");
-
-  @ApiMethod(description = "update pay cron")
-  @RequestMapping(value = "/cron/pay", method = RequestMethod.PUT)
-  public ApiResult<?> pay(@RequestParam(name = "cron") @NotBlank String cron) {
-    return reschedule(payTrigger, PAY_TRIGGER_KEY, cron);
-  }
-
-  @ApiMethod(description = "update query cron")
-  @RequestMapping(value = "/cron/query", method = RequestMethod.PUT)
-  public ApiResult<?> query(@RequestParam(name = "cron") @NotBlank String cron) {
-    return reschedule(queryTrigger, QUERY_TRIGGER_KEY, cron);
+  @ApiMethod(description = "update cron")
+  @RequestMapping(value = "/cron/{jobName}", method = RequestMethod.PUT)
+  public ApiResult<?> pay(HttpServletRequest request, @PathVariable("jobName") JobName jobName,
+      @RequestParam(name = "cron") @NotBlank String cron) {
+    if (!Objects.equals(Role.ADMIN, request.getAttribute("remituser"))) return ApiResult.forbidden();
+    Tuple2<CronTriggerFactoryBean, TriggerKey> tuple = JOB_MAP.get(jobName);
+    if (Objects.isNull(tuple)) return ApiResult.badRequest("invalid job");
+    return reschedule(tuple.f, tuple.s, cron);
   }
 
   public ApiResult<?> reschedule(CronTriggerFactoryBean trigger, TriggerKey key, String cron) {
@@ -72,6 +80,12 @@ public class CronController {
       return ApiResult.internalError(errMsg);
     }
     return ApiResult.ok();
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    JOB_MAP.put(JobName.pay, new Tuple2<>(payTrigger, new TriggerKey("payTrigger")));
+    JOB_MAP.put(JobName.query, new Tuple2<>(queryTrigger, new TriggerKey("queryTrigger")));
   }
 
 }
