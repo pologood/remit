2220f85f87e2cf5f7db98f4aba8aa34b952f74df
kinghost <kingraser@gmail.com>
2016-07-28 14:43:59 +0800

    authority

--- src/main/java/com/sogou/pay/remit/api/JobController.java
+++ src/main/java/com/sogou/pay/remit/api/JobController.java
@@ -7,6 +7,8 @@ package com.sogou.pay.remit.api;
 
 import java.util.Objects;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.jsondoc.core.annotation.Api;
 import org.jsondoc.core.annotation.ApiMethod;
 
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sogou.pay.remit.entity.User.Role;
 import com.sogou.pay.remit.job.TransferJob;
 import com.sogou.pay.remit.model.ApiResult;
 
@@ -32,14 +35,15 @@ public class JobController {
 
   @ApiMethod(description = "run job")
   @RequestMapping(value = "/job/{jobName}", method = RequestMethod.GET)
-  public ApiResult<?> run(@PathVariable JobName jobName) {
-    if (Objects.equals(JobName.PAY, jobName)) transferJob.pay();
-    else if (Objects.equals(JobName.QUERY, jobName)) transferJob.query();
+  public ApiResult<?> run(HttpServletRequest request, @PathVariable JobName jobName) {
+    if (!Objects.equals(Role.ADMIN, request.getAttribute("remituser"))) return ApiResult.forbidden();
+    if (Objects.equals(JobName.pay, jobName)) transferJob.pay();
+    else if (Objects.equals(JobName.query, jobName)) transferJob.query();
     return ApiResult.ok();
   }
 
   public enum JobName {
-    PAY, QUERY;
+    pay, query;
   }
 
 }
