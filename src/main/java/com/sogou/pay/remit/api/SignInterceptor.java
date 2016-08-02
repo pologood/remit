ea77e92b9574bccc20820ccda988ff540bb1cdbc
kinghost <kingraser@gmail.com>
2016-08-02 10:50:48 +0800

    refactor: get key from header

--- src/main/java/com/sogou/pay/remit/api/SignInterceptor.java
+++ src/main/java/com/sogou/pay/remit/api/SignInterceptor.java
@@ -6,24 +6,28 @@
 package com.sogou.pay.remit.api;
 
 import java.io.PrintWriter;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.google.common.collect.Sets;
 import com.sogou.pay.remit.enums.Exceptions;
 import com.sogou.pay.remit.manager.AppManager;
 import com.sogou.pay.remit.model.ApiResult;
 
 import commons.utils.JsonHelper;
+import commons.utils.SignHelper;
 
 //--------------------- Change Logs----------------------
 //@author wangwenlong Initial Created at 2016年7月18日;
@@ -33,18 +37,27 @@ public class SignInterceptor extends HandlerInterceptorAdapter {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    Map<String, Object> map = new HashMap<>();
+    if (!FILTERED_METHOD.contains(request.getMethod())) return true;
+    String theirSign = request.getHeader(PANDORA_SIGN);
+    if (StringUtils.isNotBlank(theirSign) && Objects.equals(theirSign, sign(request))) return true;
+    writeResponse(response, ApiResult.badRequest(Exceptions.SIGN_INVALID));
+    return false;
+  }
 
+  private String sign(HttpServletRequest request) throws Exception {
+    String sign = null;
     if (RequestMethod.GET.name().equalsIgnoreCase(request.getMethod())) {
-      Enumeration<String> params = request.getParameterNames();
-      for (String s; params.hasMoreElements(); map.put(s = params.nextElement(), request.getParameter(s)));
-    } else if (RequestMethod.POST.name().equalsIgnoreCase(request.getMethod()))
-      map = JsonHelper.toMap(IOUtils.toString(request.getReader()));
-
-    if (MapUtils.isNotEmpty(map) && !AppManager.checkSign(map)) {
-      writeResponse(response, ApiResult.badRequest(Exceptions.SIGN_INVALID));
-      return false;
-    } else return true;
+      Map<String, String> map = new HashMap<>();
+      request.getParameterMap().entrySet().stream()
+          .filter(e -> Objects.nonNull(e.getValue()) && e.getValue().length > 0)
+          .forEach(e -> map.put(e.getKey(), e.getValue()[0]));
+      sign = AppManager.sign(map);
+    } else if (RequestMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
+      String context = IOUtils.toString(request.getReader());
+      sign = SignHelper.sign(context,
+          AppManager.getKey(MapUtils.getInteger(JsonHelper.toMap(context), AppManager.APP_ITEM)));
+    }
+    return sign;
   }
 
   public static void writeResponse(HttpServletResponse response, ApiResult<?> result) throws Exception {
@@ -56,5 +69,10 @@ public class SignInterceptor extends HandlerInterceptorAdapter {
       writer.close();
     }
   }
+
+  private static final String PANDORA_SIGN = "pandorasign";
+
+  private static final Set<String> FILTERED_METHOD = Sets.newHashSet(RequestMethod.GET.name(),
+      RequestMethod.POST.name());
 
 }
